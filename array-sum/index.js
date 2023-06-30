2129adeabd4101cad38cb97119cc8c3463e2b289
Sensaz <klaudiusz.wiktor1@gmail.com>
2023-06-30 13:39:11 +0200

    solved array sum problem

---
+++ array-sum/index.js
@@ -0,0 +1,18 @@
+//  array_sum([1,2,[3,4,[5]]])
+
+function array_sum(arr, currentSum = 0) {
+  debugger;
+  let sum = currentSum;
+  arr.forEach((number) => {
+    if (typeof number === "number") sum += number;
+    else if (typeof number === "object") sum = array_sum(number, sum);
+    else return;
+  });
+  return sum;
+}
+
+array_sum([1, 2, [3, 4, [5]]]);
+
+// .reduce((a, b) => parseInt(a) + parseInt(b));
+
+// za pomocą is type, jeśli jest array to wywołaj rekurencyjnie funkcje i przeka tą arrayke
